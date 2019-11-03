272e9b0cb9f8538fcb39f8fef99b506884bd57e5
Vincent <vinnylin721@gmail.com>
2019-11-02 20:08:58 -0400

    added stuff

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,4 @@
-const Note = require('../models/model')
-const Post = require('../models/model')
+const Product = require('../models/model')
 
 exports.test = (req, res) => {
     res.send('Greetings from the test controller')
@@ -11,30 +10,16 @@ exports.note_create = (req, res) => {
         date: req.body.date
     })
 
-    note.save()
-        .then(doc => {
-            console.log(doc)
-        })
-        .catch(err => {
-            console.error(err)
-        })
-}
-
-exports.post_create = (req, res) => {
-    let post = new Post({
-        author: req.body.author,
-        date: req.body.date,
+    note.save((err) => {
+        if (err) {
+            return next(err)
+        }
+        res.send('Note created successfully')
     })
 }
 
-<<<<<<< HEAD
 exports.note_get = (req, res) => {
   Product.findById(req.body.author, function(product){
     res.send(product.value)
   })
 }
-=======
-exports.test_create = (req, res) => {
-    res.json({ file: req.file })
-}
->>>>>>> 23d006f4fdd5ab1f2b486bfa0b1ce45bf7a7ecb4
